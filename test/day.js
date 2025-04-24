b1feab0b0e2ccaca2d8e2dc3d5087069710f14c3
wangzirui <wangzirui@taou.com>
2025-04-24 15:15:19 +0800

    1

--- test/day.js
+++ test/day.js
@@ -3,3 +3,9 @@ import dayjs from "dayjs";
 console.log(dayjs("2025-3-1").daysInMonth());
 console.log(dayjs("2025-3-2").startOf("month").format("YYYY-MM-DD"));
 console.log(dayjs("2025-3-3").endOf("month").format("YYYY-MM-DD"));
+
+console.log(dayjs("2025-3-1").daysInMonth());
+console.log(dayjs("2025-3-2").startOf("month").format("YYYY-MM-DD"));
+console.log(dayjs("2025-3-3").endOf("month").format("YYYY-MM-DD"));
+
+
